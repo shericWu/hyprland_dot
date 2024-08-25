3dd4182601a807d4146a8235cc9e9e65a045c1b5
shericWu <shihyu1127@gmail.com>
2024-08-26 01:31:39 +0800

    [hyprland] modify createWS for multi monitor support

--- private_dot_config/hypr/script/executable_createWS.sh
+++ private_dot_config/hypr/script/executable_createWS.sh
@@ -1,18 +1,42 @@
 #!/usr/bin/env bash
 
-HYPRLAND_WSCOUNT=9
+MONITOR_NAME=$(hyprctl monitors -j | jq '.[].name')
+MONITOR_COUNT=$(wc --lines <<< "$MONITOR_NAME")
+MONITOR=()
+IFS=$'\n'
+read -rd '' -a MONITOR <<< "$MONITOR_NAME"
 
 WSRULES=""
 WSCREATE=""
-for (( i=HYPRLAND_WSCOUNT; i>=1; i-- ));
+HYPRLAND_WSCOUNT=9
+for (( m=0; m<MONITOR_COUNT; m++ ));
 do
-    WSRULES+="keyword workspace $i,persistent:true; "
-    WSCREATE+="dispatch workspace $i; "
+    oldName=${MONITOR[m]}
+    name="${oldName:1:-1}"
+    id=$((m + 1))
+    for (( i=1; i<=HYPRLAND_WSCOUNT; i++ ));
+    do
+        WSRULES+="keyword workspace $id,persistent:true,monitor:$name; "
+        WSCREATE+="dispatch workspace $id; "
+        id=$((id + MONITOR_COUNT))
+    done
 done
 
+echo "$WSRULES $WSCREATE"
 hyprctl --batch "$WSRULES$WSCREATE"
 
-hyprctl dispatch workspace 5
-sleep 0.1
-hyprctl dispatch workspace 1
-hyprctl dispatch workspace 5
+MIDDLE=$((((HYPRLAND_WSCOUNT+1) / 2 - 1) * MONITOR_COUNT))
+for (( m=1; m<=MONITOR_COUNT; m++ ));
+do
+    echo "hyprctl dispatch workspace $ID"
+    hyprctl dispatch workspace "$((m + MIDDLE))"
+    sleep 0.1
+    hyprctl dispatch workspace "$m"
+    hyprctl dispatch workspace "$((m + MIDDLE))"
+done
+
+hyprctl dispatch workspace "$((1 + MIDDLE))"
+# hyprctl dispatch workspace e~5
+# sleep 0.1
+# hyprctl dispatch workspace e~1
+# hyprctl dispatch workspace e~5
