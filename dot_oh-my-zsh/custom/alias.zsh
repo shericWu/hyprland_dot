420008575ea0c60778ed5cf59f52e39eb23bebd1
shericWu <shihyu1127@gmail.com>
2024-09-08 16:05:27 +0800

    [zathura] add normal color and alias opdf

--- dot_oh-my-zsh/custom/alias.zsh
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -1,2 +1,3 @@
 alias chedit='chezmoi edit --apply'
 alias swwwimg='swww img -t any --transition-step=4 --transition-duration=2 --transition-fps=60'
+alias opdf='zathura --fork -c ~/.config/zathura.normal'
