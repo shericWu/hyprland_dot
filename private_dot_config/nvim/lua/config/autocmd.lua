5ba7b564d1083d3bfbb9fc5bbeca11e9153c3e50
shericWu <shihyu1127@gmail.com>
2024-09-12 21:43:52 +0800

    [nvim] add autocmd for makefile to not expand tab

--- private_dot_config/nvim/lua/config/autocmd.lua
+++ private_dot_config/nvim/lua/config/autocmd.lua
@@ -7,3 +7,17 @@ vim.api.nvim_create_autocmd({"CursorMoved"}, {
     pattern = { "*.tex" },
     command = "VimtexView"
 })
+
+vim.api.nvim_create_autocmd({"BufEnter"}, {
+    pattern = { "Makefile", "makefile" },
+    callback = function ()
+        vim.opt.expandtab = false
+    end
+})
+
+vim.api.nvim_create_autocmd({"BufLeave"}, {
+    pattern = { "Makefile", "makefile" },
+    callback = function ()
+        vim.opt.expandtab = true
+    end
+})
